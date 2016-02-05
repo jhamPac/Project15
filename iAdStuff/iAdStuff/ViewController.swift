2a1df333bea2be9961e9a5e569aa7b87d6a3f1b5
Kevia <cloud@kevia.me>
2016-02-05 11:59:32 -0800

    project setup

---
+++ iAdStuff/iAdStuff/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  iAdStuff
+//
+//  Created by jhampac on 2/5/16.
+//  Copyright © 2016 jhampac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
+
